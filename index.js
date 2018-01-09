aa339f2f39a58991a85fcc653ec93fcad98fac33
alcic <alessiaciccarello@hotmail.it>
2018-01-09 16:48:56 +0100

    added a new function and some minor changers

--- index.js
+++ index.js
@@ -2,35 +2,34 @@ var users= [
   {
     name: 'Gino',
     surname: 'Paoli',
-    email: 'ginopaoli@gmail.com',
-    id:1.--.
+    id:1
   },
   {
     name: 'Renato',
     surname: 'Carosone',
-    email: 'renatocarosone@gmail.com',
     id:2
   },
   {
     name: 'Rita',
     surname: 'Pavone',
-    email: 'ritapavone@gmail.com',
     id:3
   },
   {
     name: 'Adriano',
     surname: 'Celentano',
-    email: 'adrianocelentano@gmail.com',
     id:4
   },
   {
     name: 'Caterina',
     surname: 'Caselli',
-    email: 'caterinacaselli@gmail.com',
     id:5
   }]
 
-var backup = JSON.parse(JSON.stringify(users));
+var clone= function(obj){
+  return JSON.parse(JSON.stringify(obj));
+}
+
+var backup = clone(users);
 
 exports.getUsers= function(){
   return users;
@@ -78,7 +77,10 @@ exports.modifyUser = function(id, newUser) {
 }
 
 exports.reset = function() {
-    users = JSON.parse(JSON.stringify(backup)); //è un modo per creare un clone e ingannare java
+    users = clone(backup); //è un modo per creare un clone e ingannare java
 }
 
-console.log(this.getUsers())
+console.log(this.addUser({name:"Alessia", surname:"Ciccarello"}));
+console.log(this.getUsers());
+console.log(this.reset());
+console.log(this.getUsers());
